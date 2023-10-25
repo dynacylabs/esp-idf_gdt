e0c0cc8f57be4696351c6eb41d9230845b2bab48
Austin Conn <austinc3030@gmail.com>
2023-10-25 11:14:10 -0400

    Update script

--- generate_all.py
+++ generate_all.py
@@ -5,7 +5,7 @@ import os
 import subprocess
 
 ghidra_project_basename = "gdt-"
-gdt_out_child_base_dirname = "esp-idf-"
+gdt_library_prefix = "lib_"
 
 # Get current dir
 base_dir = os.getcwd()
@@ -16,9 +16,12 @@ ghidra_install_path = os.path.join(base_dir, "ghidra")
 ghidra_gdt_path = os.path.join(base_dir, "ghidra-gdt")
 ghidra_projects_path = os.path.join(base_dir, "ghidra_projects")
 ghidra_headless_path = os.path.join(ghidra_install_path, "support", "analyzeHeadless")
+gdt_libs_path = os.path.join(base_dir, "libraries")
 
 if not os.path.exists(ghidra_projects_path):
     os.mkdir(ghidra_projects_path)
+if not os.path.exists(gdt_libs_path):
+    os.mkdir(gdt_libs_path)
 
 release_list = ['release_v2.0',
                 'release_v2.1',
@@ -39,6 +42,7 @@ for release_dir in release_list:
     out_dir = os.path.join(base_dir, release_dir)
     if not os.path.exists(out_dir):
         os.mkdir(os.path.join(base_dir, release_dir))
+    gdt_lib_file = os.path.join(gdt_libs_path, (gdt_library_prefix + release_dir + ".gdt"))
 
     os.chdir(os.path.join(esp_idf_elfs_path, release_dir))
     elf_glob = glob.glob('*.elf')
@@ -52,6 +56,7 @@ for release_dir in release_list:
                    ghidra_project_name,
                    "-import", elf_path,
                    "-scriptPath", ghidra_gdt_path,
-                   "-postScript", "ExportGDT.py", out_file] 
+                   "-postScript", "ExportGDT.py", out_file,
+                   "-postScript", "ExportGDT.py", gdt_lib_file] 
 
         subprocess.run(command)
